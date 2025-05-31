fcd09913735ef0640a4b0affabd3f237e16f7cfa
ttrriipp <136776800+ttrriipp@users.noreply.github.com>
2025-05-31 09:41:24 +0800

    wew

---
+++ lua/custom/plugins/oil.lua
@@ -0,0 +1,12 @@
+vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+
+return {
+  'stevearc/oil.nvim',
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {},
+  -- Optional dependencies
+  dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+  -- dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if you prefer nvim-web-devicons
+  lazy = false,
+}
---
+++ lua/custom/plugins/remap.lua
@@ -0,0 +1,6 @@
+vim.keymap.set('x', '<leader>v', '+p')
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('x', '<leader>p', [["_dP]])
+
+return {}
